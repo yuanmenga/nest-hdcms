43c603999e774ba251f392b24226535305e1c75c
yuanmenga <2243433995@qq.com>
2023-06-27 22:49:16 +0800

    新建nest框架，初始化数据库连接

---
+++ nest/nest.md
@@ -0,0 +1,5 @@
+技术栈：node.js、nvm （node版本管理）、nrm（国内镜像）、Mysql（数据库）、pnpm（包管理器）
+
+工具库：数据库管理Prisma、
+
+primsa使用流程：初始化prisma，更改数据库配置并连接，执行prisma文件生成数据表，生成迁移文件
---
+++ nest/prisma/migrations/20230627144257_/migration.sql
@@ -0,0 +1,12 @@
+-- CreateTable
+CREATE TABLE `User` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(191) NOT NULL,
+    `password` VARCHAR(191) NOT NULL,
+    `email` VARCHAR(191) NULL,
+    `mobile` VARCHAR(191) NULL,
+    `createTime` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updateTime` DATETIME(3) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
