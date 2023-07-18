aa0bb8815156429f2952cd156f4afdfdff5bd161
yuanmenga <2243433995@qq.com>
2023-07-18 18:53:06 +0800

    revert版本4

--- nest/prisma/migrations/20230703124230_/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `jianjie` on the `book` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE `book` DROP COLUMN `jianjie`;
--- nest/prisma/migrations/20230703124520_/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - Added the required column `jianjie` to the `Book` table without a default value. This is not possible if the table is not empty.
-
-*/
--- AlterTable
-ALTER TABLE `book` ADD COLUMN `jianjie` VARCHAR(191) NOT NULL;
